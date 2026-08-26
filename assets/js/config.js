7131346837c5fa28a4e92606a4a228f1e0f93be9
adi-0704 <adi-0704@users.noreply.github.com>
2026-08-26 11:58:34 +0530

    chore: wire Supabase project (URL + anon key) into config

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,8 +4,8 @@
 //   ga4Id / pixelId               -> analytics
 window.CH_ANALYTICS = window.CH_ANALYTICS || { ga4Id: "", pixelId: "" };
 window.CH_API = window.CH_API || {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://tfdnmlzvktqjwjjbkoif.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmZG5tbHp2a3RxandqamJrb2lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3MDU1NTEsImV4cCI6MjEwMzI4MTU1MX0.6MiLbqMoyXDio_KwSkgeM3Iev4B0pKACJMKglUf3RkY",
   base: "",
   adminKey: "collabhive"
 };
